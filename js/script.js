a657fad7b661c69598d1037db718e1feece3f5ce
felix <luisfelixtv@outlook.es>
2024-01-19 15:21:43 -0500

    procss-cv

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+
+class Coffee {
+  constructor() {
+      this.empty = true;
   }
+
+  refill() {
+      console.log("Refilling the coffee.");
+      this.empty = false;
+  }
+
+  drink() {
+      console.log("Drinking the coffee.");
+      this.empty = true;
+  }
+}
+
+const coffee = new Coffee();
+
+if (coffee.empty) {
+  coffee.refill();
+} else {
+  coffee.drink();
+}
